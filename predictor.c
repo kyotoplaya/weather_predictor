4f32f76ce2761cedd641da2c3e2a5f6a26ea2fd0
kyotoplaya <homarucom@gmail.com>
2026-06-01 23:08:41 +0300

    Корректно переключается второй экран, корректно выводяться показания. Не происходит обновления массива данных. Нужно разобраться с таймерами

--- predictor.c
+++ predictor.c
@@ -11,7 +11,9 @@
 #define TAG "Predictor"
 
 typedef enum {
-    PredictorViewMain,
+    MainView,
+    TempGraphView,
+
 } PredictorView;
 
 typedef enum {
@@ -21,29 +23,43 @@ typedef enum {
 
 typedef struct {
     ViewDispatcher* view_dispatcher;
+
     View* main_view;
+    View* temp_graph_view;
+
     FuriTimer* time_timer;
     FuriTimer* bme_timer;
+
+    int32_t temperature;
+    int32_t pressure;
+
 } PredictorApp;
 
 typedef struct {
     int32_t temperature;
     int32_t pressure;
-    int32_t humidity; // Реализовать когда придет датчик
-    int32_t ppm; // Реализовать когда придет датчик
+} MainViewModel;
+
+typedef struct {
+    int32_t temperature;
+    int32_t pressure;
 
     int temperature_list[10];
     int pressure_list[10];
-
-} PredictorModel;
+} GraphViewModel;
 
 static uint32_t predictor_exit_navigation_callback(void* context) {
     UNUSED(context);
     return VIEW_NONE;
 }
 
-static void predictor_draw_callback(Canvas* canvas, void* model) {
-    PredictorModel* m = model;
+static uint32_t predictor_back_to_main_callback(void* context) {
+    UNUSED(context);
+    return MainView;
+}
+
+static void predictor_draw_main_callback(Canvas* canvas, void* model) {
+    MainViewModel* m = model;
 
     DateTime dt;
     furi_hal_rtc_get_datetime(&dt);
@@ -65,7 +81,7 @@ static void predictor_draw_callback(Canvas* canvas, void* model) {
     snprintf(buf, sizeof(buf), "%ld.%ld C", m->temperature / 10, labs(m->temperature % 10));
     canvas_draw_str(canvas, 5, 42, buf);
 
-    snprintf(buf, sizeof(buf), "%d mmHg", (int)(m->pressure / 133.3));
+    snprintf(buf, sizeof(buf), "%ld mmHg", m->pressure);
     canvas_draw_str(canvas, 5, 56, buf);
 
     // Временное решение ввиду отсутствия BME280
@@ -73,6 +89,26 @@ static void predictor_draw_callback(Canvas* canvas, void* model) {
 
     // Временное решение ввиду отсутствия датчика CO2
     canvas_draw_str(canvas, 80, 56, "407 ppm");
+}
+
+static void predictor_draw_temp_graph_callback(Canvas* canvas, void* model) {
+    GraphViewModel* m = model;
+
+    canvas_clear(canvas);
+
+    char buf[32];
+
+    canvas_set_font(canvas, FontPrimary);
+
+    for(int i = 0; i < 5; i++) {
+        snprintf(buf, sizeof(buf), "%d", m->temperature_list[i]);
+        canvas_draw_str(canvas, 5, 11 * (i + 1), buf);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        snprintf(buf, sizeof(buf), "%d", m->temperature_list[i]);
+        canvas_draw_str(canvas, 50, 11 * (i + 1), buf);
+    }
 }
 
 static void time_timer_callback(void* context) {
@@ -123,15 +159,34 @@ static bool predictor_custom_event_callback(uint32_t event, void* context) {
 
     switch(event) {
     case TimeEventRedraw:
-        with_view_model(app->main_view, PredictorModel * model, { UNUSED(model); }, true);
+        with_view_model(app->main_view, MainViewModel * model, { UNUSED(model); }, true);
         return true;
     case BMEEventRedraw:
         with_view_model(
             app->main_view,
-            PredictorModel * model,
+            MainViewModel * model,
             {
                 model->temperature = get_temperature();
-                model->pressure = get_pressure();
+                model->pressure = (int)(get_pressure() / 133.3);
+            },
+            true);
+
+        with_view_model(
+            app->temp_graph_view,
+            GraphViewModel * model,
+            {
+                model->temperature = get_temperature();
+                model->pressure = (int)(get_pressure() / 133.3);
+
+                for(int i = 0; i < 9; i++) {
+                    model->temperature_list[i] = model->temperature_list[i + 1];
+                }
+                model->temperature_list[9] = model->temperature;
+
+                for(int i = 0; i < 9; i++) {
+                    model->pressure_list[i] = model->pressure_list[i + 1];
+                }
+                model->pressure_list[9] = model->pressure;
             },
             true);
         return true;
@@ -139,6 +194,18 @@ static bool predictor_custom_event_callback(uint32_t event, void* context) {
     default:
         return false;
     }
+}
+
+static bool predictor_input_callback(InputEvent* event, void* context) {
+    PredictorApp* app = (PredictorApp*)context;
+    if(event->type == InputTypeShort) {
+        if(event->key == InputKeyRight) {
+            view_dispatcher_switch_to_view(app->view_dispatcher, TempGraphView);
+
+            return true;
+        }
+    }
+    return false;
 }
 
 static PredictorApp* predictor_app_alloc(void) {
@@ -157,41 +224,65 @@ static PredictorApp* predictor_app_alloc(void) {
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
 
     app->main_view = view_alloc();
+    app->temp_graph_view = view_alloc();
 
     view_set_context(app->main_view, app);
 
-    view_allocate_model(app->main_view, ViewModelTypeLockFree, sizeof(PredictorModel));
+    view_allocate_model(app->main_view, ViewModelTypeLockFree, sizeof(MainViewModel));
+    view_allocate_model(app->temp_graph_view, ViewModelTypeLockFree, sizeof(GraphViewModel));
 
     with_view_model(
         app->main_view,
-        PredictorModel * model,
+        MainViewModel * model,
         {
             model->temperature = get_temperature();
-            model->pressure = get_pressure();
+            model->pressure = (int)(get_pressure() / 133.3);
         },
         false);
 
-    view_set_draw_callback(app->main_view, predictor_draw_callback);
+    with_view_model(
+        app->temp_graph_view,
+        GraphViewModel * model,
+        {
+            model->temperature = get_temperature();
+            model->pressure = (int)(get_pressure() / 133.3);
+
+            for(int i = 0; i < 10; i++) {
+                model->temperature_list[i] = model->temperature;
+                model->pressure_list[i] = model->pressure;
+            }
+        },
+        false);
+
+    view_set_draw_callback(app->main_view, predictor_draw_main_callback);
+    view_set_draw_callback(app->temp_graph_view, predictor_draw_temp_graph_callback);
+
+    view_set_input_callback(app->main_view, predictor_input_callback);
+    view_set_input_callback(app->temp_graph_view, predictor_input_callback);
 
     view_set_enter_callback(app->main_view, predictor_enter_callback);
 
     view_set_exit_callback(app->main_view, predictor_exit_callback);
 
     view_set_previous_callback(app->main_view, predictor_exit_navigation_callback);
+    view_set_previous_callback(app->temp_graph_view, predictor_back_to_main_callback);
 
     view_set_custom_callback(app->main_view, predictor_custom_event_callback);
 
-    view_dispatcher_add_view(app->view_dispatcher, PredictorViewMain, app->main_view);
+    view_dispatcher_add_view(app->view_dispatcher, MainView, app->main_view);
+    view_dispatcher_add_view(app->view_dispatcher, TempGraphView, app->temp_graph_view);
 
-    view_dispatcher_switch_to_view(app->view_dispatcher, PredictorViewMain);
+    view_dispatcher_switch_to_view(app->view_dispatcher, MainView);
 
     return app;
 }
 
 static void predictor_app_free(PredictorApp* app) {
-    view_dispatcher_remove_view(app->view_dispatcher, PredictorViewMain);
+    view_dispatcher_remove_view(app->view_dispatcher, MainView);
+    view_dispatcher_remove_view(app->view_dispatcher, TempGraphView);
 
     view_free(app->main_view);
+    view_free(app->temp_graph_view);
 
     view_dispatcher_free(app->view_dispatcher);
 
